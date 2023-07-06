eacf26d1e96a36967d3863407605d3ad24a3aa4c
why-1513 <1138828080@qq.com>
2023-07-06 19:00:17 +0800

    新增用链表完成josephus

---
+++ josephus_linked_list.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def create_circular_linked_list(total_num):
+    head = Node(1)
+    node_current = head
+    for i in range(2, total_num + 1):
+        node_new = Node(i)
+        node_current.next = node_new
+        node_current = node_new
+
+    node_current.next = head
+    return head
+
+
+class Josephus_ring:
+    def __init__(self):
+        self.head = None
+        self.total_num = int(input("请输入总人数："))
+        self.step_num = int(input("请输入循环的数："))
+
+    def josephus_ring_running(self):
+        if self.total_num < 1 or self.step_num < 1:
+            return None
+        self.head = create_circular_linked_list(self.total_num)
+        inc_count = 0
+        out_list = []
+        # 定位到最后一个节点
+        prev = self.head
+        while prev.next != self.head:
+            prev = prev.next
+
+        while self.head != self.head.next:
+            for _ in range(self.step_num-1):
+                prev = self.head
+                self.head = self.head.next
+            prev.next = self.head.next
+            out_list.append(self.head.value)
+            self.head = self.head.next
+        print('出局顺序：{}'.format(out_list))
+        print('最后生还者:{}'.format(self.head.value))
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from josephus_list import *
+from josephus_linked_list import *
 import time
 
 
 if __name__ == '__main__':
+    # 测试josephus_list
+    """
     total_num, step_num = josephus_list_input()
     start_time = time.perf_counter()
     josephus_list(total_num, step_num)
     end_time = time.perf_counter()
     run_time = end_time-start_time
+    print('josephus_list运行时间:{}'.format(run_time))
+    """
+    # 测试josephus_linked_list
+    josephus_ring_ = Josephus_ring()
+    start_time = time.perf_counter()
+    josephus_ring_.josephus_ring_running()
+    end_time = time.perf_counter()
+    run_time = end_time - start_time
     print('josephus_list运行时间:{}'.format(run_time))
