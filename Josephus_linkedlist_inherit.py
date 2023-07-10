3b1c727c87cdf4c1a4e04779c34b529b571682ac
why-1513 <1138828080@qq.com>
2023-07-10 18:25:18 +0800

    增加了继承和迭代器

---
+++ Josephus_linkedlist_inherit.py
@@ -0,0 +1,100 @@
+class Person:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class JosephusLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def add_person(self, value):
+        new_person = Person(value)
+        if not self.head:
+            self.head = new_person
+            new_person.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+
+            temp.next = new_person
+            new_person.next = self.head
+
+        self.length += 1
+
+    def delete_person(self, value):
+        if not self.head:
+            print("链表为空，不能删除节点。")
+            return
+        if self.head.value == value:
+            if self.head == self.head.next:
+                self.head = None
+            else:
+                temp = self.head
+                while temp.next != self.head:
+                    temp = temp.next
+                temp.next = self.head.next
+                self.head = self.head.next
+                self.length -= 1
+            return
+
+        temp = self.head
+        while temp.next != self.head and temp.next.value != value:
+            temp = temp.next
+
+        if temp.next == self.head:
+            print("没有找到要删除的节点。")
+        else:
+            temp.next = temp.next.next
+            self.length -= 1
+
+    def display(self):
+        if not self.head:
+            print("链表为空。")
+            return
+        temp = self.head
+        while True:
+            print(temp.value)
+            temp = temp.next
+            if temp == self.head:
+                break
+        print()
+
+
+class Josephus(JosephusLinkedList):
+    def __init__(self, step_num):
+        super().__init__()
+        self.step = step_num
+        self.out_list = []
+        self.survivor = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.head is None:
+            raise StopIteration
+
+        if self.length == 1:
+            value = self.head.value
+            self.delete_person(self.head.value)
+            self.survivor = value
+            return value
+
+        temp = self.head
+
+        for i in range(self.step - 1):
+            temp = temp.next
+
+        value = temp.value
+        self.delete_person(value)
+        self.out_list.append(value)
+        self.head = temp.next
+        return value
+
+    def josephus_print(self):
+        print('出局顺序：{}'.format(self.out_list))
+        print('幸存者:{}'.format(self.survivor))
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from josephus_list import *
 from josephus_linked_list import *
+from Josephus_linkedlist_inherit import *
 import time
 from faker import Faker
 
 
 if __name__ == '__main__':
+    josephus = Josephus(3)
+    for i in range(1, 9):
+        josephus.add_person(i)
+
+    for person in josephus:
+        print(person)
+    josephus.josephus_print()
     # 测试josephus_list
     """
     total_num, step_num = josephus_list_input()
@@ -14,7 +22,7 @@ if __name__ == '__main__':
     run_time = end_time-start_time
     print('josephus_list运行时间:{}'.format(run_time))
     """
-
+    """
     # 测试josephus_linked_list
     josephus_ring_ = Josephus_ring()
     start_time = time.perf_counter()
@@ -22,4 +30,4 @@ if __name__ == '__main__':
     end_time = time.perf_counter()
     run_time = end_time - start_time
     print('josephus_linked_list运行时间:{}'.format(run_time))
-
+    """
