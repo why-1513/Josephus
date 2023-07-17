8c0ac18cdc8ed0e9c3814040a9d7b4caa6783ac3
why-1513 <1138828080@qq.com>
2023-07-17 20:06:44 +0800

    Add TxtReader

---
+++ TxtReader.py
@@ -0,0 +1,39 @@
+import json
+import logging
+
+
+class TxtReader:
+
+    def __init__(self, filename=''):
+        self.filename = filename
+
+    def read_file(self):
+        person_infos = []
+        try:
+            with open(self.filename, 'r') as f:
+                lines = f.readlines()
+            for line in lines:
+                # 使用strip方法去除字符串两端的空白字符，使用split方法将每一行的信息按照'，'进行分隔 ['程凤兰':'女':'47']
+                info = line.strip().split(':')
+                if len(info) == 3:
+                    name, gender, age = info
+                    try:
+                        age = int(age)
+                    except ValueError:
+                        raise ValueError(f"Invalid age '{age}' on line '{line.strip()}' in file '{self.filename}'")
+
+                    if gender not in ['男', '女']:
+                        raise ValueError(f"Invalid gender '{gender}' on line '{line.strip()}' in file '{self.filename}'")
+                    info_dict = {
+                        'name': name,
+                        'gender': gender,
+                        'age': age
+                    }
+                    person_infos.append(info_dict)
+                else:
+                    raise ValueError(f"Invalid line '{line.strip()}' in file '{self.filename}'")
+
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File '{self.filename}' not found")
+
+        return person_infos
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Josephus_deque import *
 import time
 from CreateInfoFile import *
-from FileReader import *
+from TxtReader import *
 
 
 def wait_for_input_int(prompt, check_func, error_prompt):
@@ -19,13 +19,13 @@ def wait_for_input_int(prompt, check_func, error_prompt):
 
 if __name__ == '__main__':
 
-    total_num = wait_for_input_int(prompt="请输入总人数：", check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于零的数")
+    # total_num = wait_for_input_int(prompt="请输入总人数：", check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于零的数")
     step_num = wait_for_input_int(prompt="请输入淘汰的数：", check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于零的数")
     start_pos = wait_for_input_int(prompt="请输入开始的位置：", check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于等于零的数")
 
     # create_info_txt(total_num)
-    file = FileReader('info.txt')
-    info_list = file.txt_read()
+    file = TxtReader('info.txt')
+    info_list = file.read_file()
 
     josephus = Josephus(step_num, start_pos)
     total_num = len(info_list)
