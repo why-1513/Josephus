12d20efaf8540ea82aa3c0e737f10615e4b95497
why-1513 <1138828080@qq.com>
2023-07-18 16:29:52 +0800

    Add CsvReader

---
+++ CsvReader.py
@@ -0,0 +1,36 @@
+import csv
+
+
+class CsvReader:
+    def __init__(self, filename=''):
+        if not filename.endswith('.csv'):
+            raise ValueError(f"Invalid file type, '{filename}' is not a csv file")
+        self.filename = filename
+
+    def read_csv(self):
+        person_infos = []
+        try:
+            with open(self.filename, 'r', newline='') as csvfile:
+                # 每一行都被解析为一个字典，其中字典的键是CSV文件的表头，值是该行的值。
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    name = row['name']
+                    gender = row['gender']
+                    try:
+                        age = int(row['age'])
+                    except ValueError:
+                        raise ValueError(f"Invalid age '{row['age']}' in file '{self.filename}'")
+                    if gender not in ['男', '女']:
+                        raise ValueError(f"Invalid gender '{gender}' in file '{self.filename}'")
+                    info_dict = {
+                        'name': name,
+                        'gender': gender,
+                        'age': age
+                    }
+                    person_infos.append(info_dict)
+
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File '{self.filename}' not found")
+
+        return person_infos
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from CreateInfoFile import *
 from TxtReader import *
 from ZipReader import *
+from CsvReader import *
 
 
 def wait_for_input_int(prompt, check_func, error_prompt):
@@ -25,12 +26,15 @@ if __name__ == '__main__':
     start_pos = wait_for_input_int(prompt="请输入开始的位置：", check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于等于零的数")
 
     # create_info_txt(total_num)
-    # file = TxtReader('info1.txt')
-    # info_list = file.read_file()
+    file = TxtReader('info1.txt')
+    info_list = file.read_file()
 
-    file = ZipReader('info.zip')
-    file.list_files()
-    info_list = file.read_zip()
+    # file = ZipReader('info.zip')
+    # file.list_files()
+    # info_list = file.read_zip()
+
+    # csv_reader = CsvReader('info.csv')
+    # info_list = csv_reader.read_csv()
 
     josephus = Josephus(step_num, start_pos)
     total_num = len(info_list)
