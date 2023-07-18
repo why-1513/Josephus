324da9821078d4ac1b8eccc79e4a8ddc6d85b9c1
why-1513 <1138828080@qq.com>
2023-07-18 16:27:13 +0800

    add  create file.csv

--- CreateInfoFile.py
+++ CreateInfoFile.py
@@ -1,6 +1,7 @@
 from faker import Faker
 import json
 import random
+import csv
 
 
 def create_info_json(total_num):
@@ -25,9 +26,29 @@ def create_info_txt(total_num):
             'age': random.randint(18, 60)
         }
         person_infos.append(info)
-    print(person_infos)
     f = open('info.txt', 'w')
     for person in person_infos:
         f.write(f"{person['name']}:{person['gender']}:{person['age']}\n")
     f.close()
 
+
+def create_info_csv(total_num):
+    person_infos = []
+    for i in range(total_num):
+        info = {
+            'name': Faker("zh_CN").name(),
+            'gender': random.choice(['男', '女']),
+            'age': random.randint(18, 60)
+        }
+        person_infos.append(info)
+
+    with open('info.csv', 'w', newline='') as csvfile:
+        fieldnames = ['name', 'gender', 'age']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for person in person_infos:
+            writer.writerow(person)
+
+
+create_info_csv(18)
