21cbf510a2a957bbffa9d7ebd2625d3688d4a8e6
why-1513 <1138828080@qq.com>
2023-07-21 11:36:37 +0800

    Modify wait_for_input_int method

--- GetInput.py
+++ GetInput.py
@@ -1,4 +1,7 @@
-def wait_for_input_int(logger, prompt, check_func, error_prompt):
+import logging
+
+
+def wait_for_input_int(prompt, check_func, error_prompt):
     while True:
         value = input(prompt)
         try:
@@ -7,15 +10,17 @@ def wait_for_input_int(logger, prompt, check_func, error_prompt):
                 raise ValueError
             break
         except ValueError:
-            logger.log_error(error_prompt)
+            logging.error(error_prompt)
+            print(error_prompt)
     return value
 
 
-def start_get_input(logger):
-    logger.log_info('开始获取用户输入')
-    _step_num = wait_for_input_int(logger, prompt="请输入循环的数：",
+def start_get_input():
+    logging.info('开始获取用户输入')
+    _step_num = wait_for_input_int(prompt="请输入循环的数：",
                                    check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于零的数")
-    _start_pos = wait_for_input_int(logger, prompt="请输入开始的位置：",
+    _start_pos = wait_for_input_int(prompt="请输入开始的位置：",
                                     check_func=lambda x: x < 0, error_prompt="非法输入，请输入大于等于零的数")
-    logger.log_info('获取用户输入结束')
+    logging.info('获取用户输入结束')
     return _step_num, _start_pos
+
