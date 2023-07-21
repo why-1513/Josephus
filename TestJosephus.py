14a04f8aade6a10ca93061b480bfa368b212fe64
why-1513 <1138828080@qq.com>
2023-07-21 18:50:13 +0800

    add Test_report test_result

--- TestJosephus.py
+++ TestJosephus.py
@@ -79,26 +79,23 @@ class TestJosephus(unittest.TestCase):
         self.assertEqual(josephus.survivor.name, '何秀华')
 
 
-if __name__ == '__main__':
-    # 构造测试集，实例化测试套件
-    suite = unittest.TestSuite()
-    # 执行顺序为安装加载顺序
-    suite.addTest(TestJosephus("test_add_one_person"))
-    suite.addTest(TestJosephus("test_delete_person"))
-    suite.addTest(TestJosephus("test_read_person_info_from_txt"))
-    suite.addTest(TestJosephus("test_read_person_info_from_txt"))
-    suite.addTest(TestJosephus("test_read_person_info_from_zip"))
-    suite.addTest(TestJosephus("test_josephus_txt"))
-    suite.addTest(TestJosephus("test_josephus_csv"))
-    suite.addTest(TestJosephus("test_josephus_zip"))
+print(__name__)
 
-    # 执行测试，将测试结果输入到test.txt文件中
-    # with open('test_result.html', 'a') as f:
-    #     runner = unittest.TextTestRunner(stream=f, verbosity=2)
-    #     runner.run(suite)
-    try:
-        with open('testresult.txt', 'a') as f:
-            runner = unittest.TextTestRunner(stream=f, verbosity=2)
-            runner.run(suite)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+
+# if __name__ == '__main__':
+#     # 构造测试集，实例化测试套件
+#     suite = unittest.TestSuite()
+#     # 执行顺序为安装加载顺序
+#     suite.addTest(TestJosephus("test_add_one_person"))
+#     suite.addTest(TestJosephus("test_delete_person"))
+#     suite.addTest(TestJosephus("test_read_person_info_from_txt"))
+#     suite.addTest(TestJosephus("test_read_person_info_from_txt"))
+#     suite.addTest(TestJosephus("test_read_person_info_from_zip"))
+#     suite.addTest(TestJosephus("test_josephus_txt"))
+#     suite.addTest(TestJosephus("test_josephus_csv"))
+#     suite.addTest(TestJosephus("test_josephus_zip"))
+#     # 执行测试，将测试结果输入到test.txt文件中
+#     with open('test_result.txt', 'a') as f:
+#         runner = unittest.TextTestRunner(stream=f, verbosity=2)
+#         runner.run(suite)
+
